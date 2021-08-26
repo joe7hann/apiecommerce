10c2628681e1c56d5c972579c89b86b6e98a1d9d
Johann Condori <johann@gruposistemas.com>
2021-08-25 22:33:02 -0500

    commit

---
+++ app/Http/Controllers/ArticuloController.php
@@ -0,0 +1,106 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Articulo;
+
+class ArticuloController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $articulos = Articulo::all();
+        return $articulos;
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $articulo = new Articulo();
+        $articulo->name = $request->name;
+        $articulo->price = $request->price;
+        $articulo->stock = $request->stock;
+        $articulo->category = $request->category;
+        $articulo->description = $request->description;
+        $articulo->imageUrl = $request->imageUrl;
+
+        $articulo->save();
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $articulo = Articulo::findOrFail($request->id);
+        $articulo->name = $request->name;
+        $articulo->price = $request->price;
+        $articulo->stock = $request->stock;
+        $articulo->category = $request->category;
+        $articulo->description = $request->description;
+        $articulo->imageUrl = $request->imageUrl;
+
+        $articulo->save();
+
+        return $articulo;
+
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Request $request)
+    {
+        Articulo::destroy($request->id);
+        return $articulo;
+    }
+}
---
+++ database/migrations/2021_08_25_111403_create_articulos_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateArticulosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('articulos', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->float('price');
+            $table->integer('stock');
+            $table->string('category');
+            $table->string('description');
+            $table->string('imageUrl');
+            $table->timestamps();
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('articulos');
+    }
+}
